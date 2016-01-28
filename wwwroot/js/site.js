d88be7b21544572a5e625c049c99f663c314dea9
David Rinck <rinckd@gmail.com>
2016-01-28 10:57:26 -0600

    init commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+// site.js
+(function () {
+
+  var $sidebarAndWrapper = $("#sidebar,#wrapper");
+  var $icon = $("#sidebarToggle i.fa");
+
+  $("#sidebarToggle").on("click", function () {
+    $sidebarAndWrapper.toggleClass("hide-sidebar");
+    if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+      $icon.removeClass("fa-angle-left");
+      $icon.addClass("fa-angle-right");
+    } else {
+      $icon.addClass("fa-angle-left");
+      $icon.removeClass("fa-angle-right");
+    }
+  });
+})();
