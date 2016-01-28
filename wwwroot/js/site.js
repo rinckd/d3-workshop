504974b60386b853964adde445119408d80771f5
David Rinck <rinckd@gmail.com>
2016-01-28 17:51:50 -0600

    gulp

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,17 +1,17 @@
 // site.js
 (function () {
 
-  var $sidebarAndWrapper = $("#sidebar,#wrapper");
-  var $icon = $("#sidebarToggle i.fa");
+  var $sidebarAndWrapper = $('#sidebar,#wrapper');
+  var $icon = $('#sidebarToggle i.fa');
 
-  $("#sidebarToggle").on("click", function () {
-    $sidebarAndWrapper.toggleClass("hide-sidebar");
-    if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-      $icon.removeClass("fa-angle-left");
-      $icon.addClass("fa-angle-right");
+  $('#sidebarToggle').on('click', function () {
+    $sidebarAndWrapper.toggleClass('hide-sidebar');
+    if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
+      $icon.removeClass('fa-angle-left');
+      $icon.addClass('fa-angle-right');
     } else {
-      $icon.addClass("fa-angle-left");
-      $icon.removeClass("fa-angle-right");
+      $icon.addClass('fa-angle-left');
+      $icon.removeClass('fa-angle-right');
     }
   });
 })();
