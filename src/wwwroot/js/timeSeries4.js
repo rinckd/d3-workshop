cba0a6a0edf9c82013410d3641cb46ac3bae89cc
David Rinck <rinckd@gmail.com>
2016-02-19 08:36:16 -0600

    legend

--- src/wwwroot/js/timeSeries4.js
+++ src/wwwroot/js/timeSeries4.js
@@ -2,19 +2,19 @@
   var labelAxis, borderAxis, end, height, hoursAxis, hoursTickSpacing, hoursg, start, svg, timescale, width;
   width = 700;
   height = 800;
+  var legendSize = 400;
 
   var minTime = new Date(2016, 0, 1);
   var maxTime = d3.time.hour.offset(minTime, 24 * 7);
 
   end = moment().startOf('day');
   start = moment(end).subtract('days', 1);
-  timescale = d3.time.scale().nice(d3.time.day).domain([
-    start.toDate(),
-    end.toDate()
-  ]).range([
-    0,
-    width - 1
-  ]);
+  timescale = d3.time
+    .scale()
+    .nice(d3.time.day)
+    .domain([end.toDate(), start.toDate()])
+    .range([0, legendSize]);
+
   hoursAxis = d3.svg
     .axis()
     .scale(timescale)
@@ -33,14 +33,7 @@
         return null;
       }
   });
-  //borderAxis = d3.svg
-  //  .axis()
-  //  .scale(timescale)
-  //  .orient('bottom')
-  //  .ticks(d3.time.day, 1)
-  //  .tickFormat('')
-  //  .tickSize(30)
-  //  .tickPadding(6);
+
   labelAxis = d3.svg
     .axis()
     .scale(timescale)
@@ -60,34 +53,30 @@
     .classed('dayAxis', true)
     .classed('hours', true)
     .classed('labeled', true)
-    .attr('transform', 'translate(30.5,600.5) rotate(-90)')
+    .attr('transform', 'translate(30.5,400.5) rotate(-90)')
     .call(hoursAxis);
 
   hoursTickSpacing = timescale(moment(start).add('hours', 1).toDate()) - timescale(start.toDate());
-
+  console.log(hoursTickSpacing);
+  var tickSpacing = 16;
   hoursg.selectAll('g.tick')
     .insert('rect', ':first-child')
     .attr('class', function (d) {
       var hours = d.getHours();
-      if (hours < 6 || hours >= 18) {
+      if (hours <= 6 || hours > 18) {
         return 'nighttime';
       } else {
         return 'daytime';
       }
     })
     .attr('x', 0)
-    .attr('width', hoursTickSpacing)
-    .attr('height', 4);
-  //svg.append('g')
-  //  .classed('dayAxis', true)
-  //  .classed('days', true)
-  //  .attr('transform', 'translate(0.5,0.5)')
-  //  .call(borderAxis);
+    .attr('width', tickSpacing)
+    .attr('height', 8);
   svg.append('g')
     .classed('dayAxis', true)
     .classed('days', true)
     .classed('labeled', true)
-    .attr('transform', 'translate(30.5,600.5) rotate(-90)')
+    .attr('transform', 'translate(30.5,400.5) rotate(-90)')
     .call(labelAxis);
 
 }());
