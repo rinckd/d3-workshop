ad516c6aab26126f67984883bd349fcd98e65151
David Rinck <rinckd@gmail.com>
2016-02-27 15:22:56 -0600

    font-awesome

--- src/wwwroot/js/webpack/entry.js
+++ src/wwwroot/js/webpack/entry.js
@@ -11,5 +11,6 @@ require('../lib/inlet.min.js');
 require('../lib/material.min.js');
 require('codemirror/lib/codemirror.css');
 require('nvd3/build/nv.d3.min.css');
+require('font-awesome/css/font-awesome.min.css');
 require('../../css/inlet.css');
 
