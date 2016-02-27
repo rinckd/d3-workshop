024bda2dc5ed69191904d67f0e6fdb86c13f4d28
David Rinck <rinckd@gmail.com>
2016-02-27 15:19:03 -0600

    Material Design

--- src/wwwroot/js/webpack/entry.js
+++ src/wwwroot/js/webpack/entry.js
@@ -1,11 +1,6 @@
 require('d3');
 require('nvd3');
 
-var $ = require('jquery');
-window.jQuery = $;
-window.$ = $;
-
-require('bootstrap');
 var CodeMirror = require('codemirror');
 window.jQuery = CodeMirror;
 window.CodeMirror = CodeMirror;
@@ -14,11 +9,7 @@ require('colorbrewer');
 require('../lib/sticker.js');
 require('../lib/inlet.min.js');
 require('../lib/material.min.js');
-
 require('codemirror/lib/codemirror.css');
 require('nvd3/build/nv.d3.min.css');
-require('bootstrap/dist/css/bootstrap.min.css');
-require('font-awesome/css/font-awesome.min.css');
-
 require('../../css/inlet.css');
 
