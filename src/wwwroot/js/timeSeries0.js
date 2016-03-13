3246506e48d612b2eae3f514f9a8bb73322d6440
David Rinck <rinckd@gmail.com>
2016-03-13 13:14:03 -0500

    update

--- src/wwwroot/js/timeSeries0.js
+++ src/wwwroot/js/timeSeries0.js
@@ -3,68 +3,57 @@
 
   var margin = {
     top: 40,
-    right: 10,
+    right: 0,
     bottom: 40,
     left: 40
   };
   var width = 600 - margin.left - margin.right;
   var height = 400 - margin.top - margin.bottom;
 
+  var svg = d3.select('#time-series')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ','
+      + margin.top + ')');
+
+  var yScale = d3.scale.linear()
+    .range([height, 0]);
+  var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient('left');
+
+  var xScale = d3.scale.linear()
+    .range([0, width]);
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient('bottom');
+
   d3.json('/data/timeseries/ac_load.json', function (error, json) {
-    if (error) {
-      return console.error(error);
-    }
-
-    var svg = d3.select('#time-series')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    var yScale = d3.scale.linear()
-      .range([height, 0])
-      .domain([0, 300]);
-
-    var yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient('left');
-
-    var xScale = d3.scale.linear()
-      .range([0, width])
-      .domain([0,23]);
-
-    var xAxis = d3.svg.axis()
-      .scale(xScale);
-
+    if (error) throw error;
+    xScale.domain([0,23]);
+    yScale.domain(d3.extent(json.data, function(data) {
+      return data.value;
+    }));
     var lineGenerator = d3.svg.line()
-      .x(function(d, iterator) { return xScale(iterator); })
-      .y(function(d) {
-        //console.log(d);
-        return yScale(d.value);
-      })
-      .interpolate('basis'); // ('linear')('cardinal')('monotone');
+      .x(function(data, iterator) { return xScale(iterator);})
+      .y(function(data) { return yScale(data.value);})
+      .interpolate('basis');
 
     svg.append('path')
-      .attr('d', lineGenerator(json.data))
-      .attr('stroke', '#3f51b5')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none');
+      .datum(json.data)
+      .attr('d', lineGenerator)
+      .attr('class', 'line');
+
     svg.append('g')
       .call(xAxis)
       .attr('class', 'axis')
-      .attr('transform', 'translate(0, ' + height  + ')');
-
+      .attr('transform', 'translate(0, ' + height + ')');
     svg.append('g')
       .call(yAxis)
       .attr('class', 'axis');
 
-    var title = json.title + ' (' + json.units + ')';
-    svg.append('text')
-      .attr('x', (width + margin.left + margin.right)/2 )
-      .attr('y', 0)
-      .attr('text-anchor', 'middle')
-      .attr('class', 'mdl-typography--headline')
-      .text(title);
-  });
+  })
+
 })();
