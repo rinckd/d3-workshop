1560539ed44c38270892cd9a5f199e1ace387b36
David Rinck <rinckd@gmail.com>
2016-03-15 08:43:33 -0500

    updates

--- src/wwwroot/js/scratchpad2.js
+++ src/wwwroot/js/scratchpad2.js
@@ -1,20 +1,73 @@
 (function() {
   'use strict';
-  const width = 700;
-  const height = 800;
 
-  var svg = d3.select('#time-series')
-    .append('svg')
-    .attr('width', 700)
-    .attr('height', 700);
+  var branches = [];
+  var initialLength = 100;
+  var baseXPoint = 240;
+  var baseYPoint = 500;
 
-  var path = svg.append('svg:path')
-    .attr('d', 'M 100 100 L300 100 L200 300z')
-    .attr('id', 'arc')
-    .style('stroke-width', 22)
-    .style('stroke', 'steelblue')
-    .style('fill', 'none');
+  var successiveBranchLengthRatio = 0.8;
+  var theta = 0.5;
+  var opacity = -1.4;
+  var maxDepth = 12;
+  var trunk = {
+    depth: 0,
+    x1: baseXPoint,
+    y1: baseYPoint,
+    x2: baseXPoint,
+    y2: baseYPoint - initialLength,
+    angle: 0,
+    length: initialLength * successiveBranchLengthRatio
+  };
 
+  function branchTree(branch) {
+    branches.push(branch);
+    if (branch.depth === maxDepth) {
+      return;
+    }
+    var leftBranch = {
+      x1: branch.x2,
+      y1: branch.y2,
+      x2: branch.x2 + branch.length * Math.sin(branch.angle),
+      y2: branch.y2 - branch.length * Math.cos(branch.angle),
+      angle: branch.angle - theta,
+      length: branch.length * successiveBranchLengthRatio,
+      depth: branch.depth + 1
+    };
+    branchTree(leftBranch);
+
+    var rightBranch = {
+      x1: branch.x2,
+      y1: branch.y2,
+      x2: branch.x2 + branch.length * Math.sin(branch.angle),
+      y2: branch.y2 - branch.length * Math.cos(branch.angle),
+      angle: branch.angle + theta,
+      length: branch.length * successiveBranchLengthRatio,
+      depth: branch.depth + 1
+    };
+    branchTree(rightBranch);
+  }
+
+  function create() {
+    var svg = d3.select('#time-series').append('svg')
+      .attr('width', 600)
+      .attr('height', 500);
+    var path = svg.selectAll('line');
+    var more = path
+      .data(branches)
+      .enter()
+      .append('line')
+      .style('stroke', '#000099')
+      .attr('x1', function (d) { return d.x1; })
+      .attr('y1', function (d) { return d.y1; })
+      .attr('x2', function (d) { return d.x2; })
+      .attr('y2', function (d) { return d.y2; })
+      .style('stroke-opacity', function (d) { return (maxDepth + opacity - d.depth) * 0.1; })
+      .style('stroke-width', function (d) { return ((maxDepth + 1 - d.depth) * 0.4) + 'px'; });
+
+  }
+  branchTree(trunk);
+  create();
 
 }());
 
